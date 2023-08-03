c19e086e468c8279af0ea735a191c5e1e8a7d380
etomlins <116319303+etomlins@users.noreply.github.com>
2023-08-03 00:50:44 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,24 @@
 function loadCSVFile() {
-    const csvFilePath = "kaggle_poem_dataset.csv";
+  const csvFilePath = "kaggle_poem_dataset.csv";
 
   Papa.parse(csvFilePath, {
     download: true,
     complete: function (parsedData) {
       const data = parsedData.data;
       console.log(data); 
-        }
-    });
+
+      const poemName = document.getElementById('form1').value.toLowerCase();
+      const foundPoem = data.find(entry => entry[2] === poemName);
+
+      if (foundPoem) {
+        const [id, author, title, number, content] = foundPoem;
+        console.log(`Found poem with title "${title}" by ${author}:`);
+        console.log(`Content: ${content}`);
+      } else {
+        console.log(`Poem titled "${poemName}" not found in the array.`);
+      }
+    }
+  });
 }
 
 loadCSVFile();
-
-const poemName = document.getElementById('form1').value.toLowerCase();
-const foundPoem = data.find(entry => entry[2] === poemName);
-
-if (foundPoem) {
-  const [id, author, title, number, content] = foundPoem;
-  console.log(`Found poem with title "${title}" by ${author}:`);
-  console.log(`Content: ${content}`);
-} else {
-  console.log(`Poem titled "${poemName}" not found in the array.`);
-}
-    
-
-
