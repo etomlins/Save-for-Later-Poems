78184b68a9481e92c3b0e82c4811bcb16d65dc21
etomlins <116319303+etomlins@users.noreply.github.com>
2023-08-03 00:42:26 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,17 @@ function loadCSVFile() {
 }
 
 loadCSVFile();
+
+const poemName = document.getElementById('searchBar').value.toLowerCase();
+const foundPoem = data.find(entry => entry[2] === poemName);
+
+if (foundPoem) {
+  const [id, author, title, number, content] = foundPoem;
+  console.log(`Found poem with title "${title}" by ${author}:`);
+  console.log(`Content: ${content}`);
+} else {
+  console.log(`Poem titled "${poemName}" not found in the array.`);
+}
+    
+
+
