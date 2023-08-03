560d716a7218ff44ce5eba398e62ce092a676705
etomlins <116319303+etomlins@users.noreply.github.com>
2023-08-03 00:29:51 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,12 @@
+function loadCSVFile() {
+    const csvFilePath = "kaggle_poem_dataset.csv";
 
-
-var results = Papa.parse("kaggle_poem_dataset.csv", {
-    download: true,
-    complete: function(results) {
-      console.log(results);
-    
-  
-console.log(results)
-
-function findPoem() {
-    const poemName = document.getElementById('searchBar').value;
-    const poem = results.find(item => item.title.toLowerCase() === poemName.toLowerCase());
-
-    const poemContentElement = document.getElementById('content');
-    if (poem) {
-        poemContentElement.textContent = poem.content;
-    } else {
-        poemContentElement.textContent = 'Poem not found.';
-    }
+    Papa.parse(csvFilePath, {
+        download: true,
+        complete: function (parsedData) {
+            console.log(parsedData.data);
+        }
+    });
 }
 
-}
-});
+loadCSVFile();
