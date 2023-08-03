982edb323aa88c44ba4c9fc5cac4c73def229f1d
etomlins <116319303+etomlins@users.noreply.github.com>
2023-08-03 14:01:38 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,37 @@
 function loadCSVFile() {
   const csvFilePath = "kaggle_poem_dataset.csv";
 
-  Papa.parse(csvFilePath, {
-    download: true,
-    complete: function (parsedData) {
-      const data = parsedData.data;
-      console.log(data); 
-
-      // make event listener for when the find poem button gets clicked
-      document.getElementbyId('find-poem-click').addEventListener("click", displayPoem);
-
-    function displayPoem() {
-        const poemName = document.getElementById('form1').value.toLowerCase();
-      const foundPoem = data.find(entry => entry[2] === poemName);
-      if (!foundPoem) {
-        console.log('did not work :(')
-      }
-      if (foundPoem) {
-        const [id, author, title, number, content] = foundPoem;
-        console.log(`Found poem with title "${title}" by ${author}:`);
-        console.log(`Content: ${content}`);
-      } else {
-        console.log(`Poem titled "${poemName}" not found in the array.`);
-      }
-
-    }
-
-      
-      
-    }
+  return new Promise((resolve, reject) => {
+    Papa.parse(csvFilePath, {
+      download: true,
+      complete: function (parsedData) {
+        const data = parsedData.data;
+        resolve(data);
+      },
+      error: function (error) {
+        reject(error);
+      },
+    });
   });
 }
 
-loadCSVFile();
+loadCSVFile()
+  .then((data) => {
+    // Data is available here
+    document.getElementById("find-poem-click").addEventListener("click", displayPoem);
+
+    function displayPoem() {
+      const poemName = document.getElementById('form1').value.toLowerCase();
+      const foundPoem = data.find(entry => entry[2] === poemName);
+      if (!foundPoem) {
+        console.log('did not work :(');
+      } else {
+        const [id, author, title, number, content] = foundPoem;
+        console.log(`Found poem with title "${title}" by ${author}:`);
+        console.log(`Content: ${content}`);
+      }
+    }
+  })
+  .catch((error) => {
+    console.error("Error loading CSV file:", error);
+  });
