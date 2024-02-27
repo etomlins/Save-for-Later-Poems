f56e2fcc89d16ad5991eb166c7fe0d5bf1850c10
Ellen Tomlins <116319303+etomlins@users.noreply.github.com>
2024-02-26 21:20:13 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
         complete: function(results) {
           csvData = results.data;
           document.getElementById("find-poem-click").addEventListener("click", function() {
+            console.log('clicked!');
             const poemName = document.getElementById('poem-search-input').value.toLowerCase();
             const foundPoem = csvData.find(entry => entry['Title'].toLowerCase().includes(poemName));
             console.log(document.getElementById('poem-search-input'));
